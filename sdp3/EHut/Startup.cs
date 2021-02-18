f07fe55154a54be588e152a1c72c6b2c04727f48
rahathosen <rahathosen.me@gmail.com>
2021-02-19 02:00:48 +0600

    update bootstrap design

--- sdp3/EHut/Startup.cs
+++ sdp3/EHut/Startup.cs
@@ -27,6 +27,7 @@ namespace EHut
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
