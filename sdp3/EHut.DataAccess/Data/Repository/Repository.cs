bc9c3b102c6ff07012e1a07e88634abdea98aaeb
rahathosen <rahathosen.me@gmail.com>
2021-02-24 15:28:31 +0600

    implement IRepository function to Repository

--- sdp3/EHut.DataAccess/Data/Repository/Repository.cs
+++ sdp3/EHut.DataAccess/Data/Repository/Repository.cs
@@ -1,10 +1,53 @@
-﻿using System;
+﻿using EHut.DataAccess.Data.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace EHut.DataAccess.Data.Repository
 {
-    class Repository
+    public class Repository<T> : IRepository<T> where T : class
     {
+        protected readonly DbContext Context;  //using database
+        internal DbSet<T> dbSet;
+
+        public Repository(DbContext context)  //Dependency Injection
+        {
+            Context = context;
+            this.dbSet = context.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public T Get(int id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderby = null, string includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter = null, string includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            T entityToRemove = dbSet.Find(id);
+            Remove(entityToRemove);
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
     }
 }
