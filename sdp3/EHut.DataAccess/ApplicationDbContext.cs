ddd46fc2714742486b5a5fca9369ff256983e73f
rahathosen <rahathosen.me@gmail.com>
2021-02-26 11:59:49 +0600

    added migration in MenuItem

--- sdp3/EHut.DataAccess/ApplicationDbContext.cs
+++ sdp3/EHut.DataAccess/ApplicationDbContext.cs
@@ -14,5 +14,6 @@ namespace EHut.DataAccess
 
         public DbSet<Category> Category { get; set; }
         public DbSet<FoodType> FoodType { get; set; }
+        public DbSet<MenuItem> MenuItem { get; set; }
     }
 }
